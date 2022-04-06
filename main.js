9d033c3f7696b5149d8a4bad8d016905766aa456
folongemerson <folongemersonwell@gmail.com>
2022-04-06 10:34:21 +0100

    commit nar

---
+++ main.js
@@ -0,0 +1,6 @@
+$('.button').click(function(){
+    
+    this.textContent = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate incidunt reprehenderit rerum at.Lorem ipsum dolor consectetur adipisicing elit. Eum, evenietihil, b Officiisuscipit inventore aperiam. Expedita libero facilis error nisi deserunt harum neque, quas commodi hic placeat illo repudiandae eos. Fugiat, quis! Pariatur, fuga"
+    
+})
+
